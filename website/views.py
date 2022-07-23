5b955e5d3cce09a0b25d7aa9282669ad345e893a
mhollen3 <mhollen3@ivytech.edu>
2022-07-23 19:48:28 -0400

    commit 2

--- website/views.py
+++ website/views.py
@@ -2,18 +2,22 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 def home():
     return render_template("home.html")
 
+
 @views.route('/pc')
 def pc():
-    return "<h1>pc</h1>"
+    return render_template("pc.html")
+
 
 @views.route('/printer')
 def printer():
-    return "<h1>printer</h1>"
+    return render_template("printer.html")
+
 
 @views.route('/phone')
 def phone():
-    return "<h1>phone</h1>"
+    return render_template("phone.html")
